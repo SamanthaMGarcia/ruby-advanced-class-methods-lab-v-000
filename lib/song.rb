2dcc273e4b48a0e43caa36d6e1ca2b975053b188
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-18 17:23:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
   end
   
   def self.create_by_name(song_name)
-    song = self.create_by_name
-    @@all << self.create_by_name
+    song = self.create
+    song.name = song_name
   end
 end
